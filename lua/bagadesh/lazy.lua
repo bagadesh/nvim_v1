76c4d50420827ae49819c454ac1c4d0b1662e3ec
bagadeshfox <bagadesh.r@fox.com>
2024-05-13 11:29:11 +0530

    update_android_stuff

--- lua/bagadesh/lazy.lua
+++ lua/bagadesh/lazy.lua
@@ -1,33 +1,34 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.g.android_sdk_path= '/Users/bagadesh.r/Library/Android/sdk'
 
 require("lazy").setup(
 {
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{ 'rose-pine/neovim', name = 'rose-pine' },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'rose-pine/neovim', name = 'rose-pine' },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
     {
         "epwalsh/obsidian.nvim",
         version = "*",
@@ -50,25 +51,28 @@ require("lazy").setup(
                     ["x"] = { char = "✔", hl_group = "ObsidianDone" },
                 },
             },
-              -- Optional, customize how note IDs are generated given an optional title.
-              ---@param title string|?
-              ---@return string
-              note_id_func = function(title)
-                  -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-                  -- In this case a note with the title 'My new note' will be given an ID that looks
-                  -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-                  local suffix = ""
-                  if title ~= nil then
-                      -- If title is given, transform it into valid file name.
-                      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-                  else
-                      -- If title is nil, just add 4 random uppercase letters to the suffix.
-                      for _ = 1, 4 do
-                          suffix = suffix .. string.char(math.random(65, 90))
-                      end
-                  end
-                  return suffix
-              end,
+            -- Optional, customize how note IDs are generated given an optional title.
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+                -- In this case a note with the title 'My new note' will be given an ID that looks
+                -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+                local suffix = ""
+                if title ~= nil then
+                    -- If title is given, transform it into valid file name.
+                    suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                else
+                    -- If title is nil, just add 4 random uppercase letters to the suffix.
+                    for _ = 1, 4 do
+                        suffix = suffix .. string.char(math.random(65, 90))
+                    end
+                end
+                return suffix
+            end,
         },
     },
+    {'hsanson/vim-android'},
 })
+
+
